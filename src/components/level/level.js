8b09ce49dffbba72f273f554a7d01bb2222385c5
Artur Tarasau <tarasov.a.web@gmail.com>
2022-08-27 08:43:32 +0300

    feat: add data

--- src/components/level/level.js
+++ src/components/level/level.js
@@ -1,15 +1,27 @@
 const amountLevels = 5
 let gameLevel = false
+const levels = ['Очень легкий' , 'Легкий уровень' , 'Средний уровень' , 'Высокий уровень' , 'Очень высокий']
+const levelAttribute = ['light' , 'easy' , 'normal' , 'hard' , 'expert']
 
 function setLevel() {
     if(!gameLevel){
         gameLevel = true
+        levels.reverse()
+        levelAttribute.reverse()
         const btnsLvl = document.createElement('div')
         const allCardsAncientDiv = document.querySelector('.cards-ancients')
         console.log(allCardsAncientDiv)
         btnsLvl.classList.add('btns-level')
 
         allCardsAncientDiv.insertAdjacentElement('afterend' , btnsLvl)
+
+        for(let i=0;i<amountLevels;i++){
+            const levelAncientBtn = document.createElement('button')
+            levelAncientBtn.textContent = levels[i]
+            levelAncientBtn.classList.add('level-btn')
+            levelAncientBtn.setAttribute('data-level' , levelAttribute[i])
+            btnsLvl.insertAdjacentElement('afterbegin' , levelAncientBtn)
+        }
     }
     
 }
