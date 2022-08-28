05f933bafe9e5f21bd29feb701f1eb4bee6c76c7
Artur Tarasau <tarasov.a.web@gmail.com>
2022-08-28 15:55:47 +0300

    deploy

--- src/components/createStore/createStore.js
+++
@@ -1,8 +0,0 @@
-import createStore from '../state/state'
-import reducer from '../reducers/reducer'
-
-let myStore = createStore(reducer)
-
-export default myStore
-
-export const dispatch = myStore.dispatch
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import "./style.css";
-import ancients from './assets/Ancients/index'
-import createCardAncient from './components/startGame/choose'
-createCardAncient(ancients)
